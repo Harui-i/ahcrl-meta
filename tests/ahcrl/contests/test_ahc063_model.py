8db180887c5a1a4240e5616b51767016637f87ce
Haruki Oshiro <68176059+Harui-i@users.noreply.github.com>
2026-08-28 13:22:19 +0900

    convnext()

--- tests/ahcrl/contests/test_ahc063_model.py
+++ tests/ahcrl/contests/test_ahc063_model.py
@@ -4,7 +4,7 @@ from ahcrl.contests.ahc063.encoder import ACTION_COUNT, MAX_BOARD_SIZE, NUM_PLAN
 from ahcrl.contests.ahc063.model import ActorCritic
 
 
-def test_actor_critic() -> None:
+def test_actor_critic_convnext() -> None:
     model = ActorCritic(NUM_PLANES, 16, 4, "convnext")
     batch_size = 5
     x = torch.randn(batch_size, NUM_PLANES, MAX_BOARD_SIZE, MAX_BOARD_SIZE)
